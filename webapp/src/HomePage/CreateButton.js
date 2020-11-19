49dba32e39127ca2a1c5cc0c27ccdb1fc3e5aebb
Sandeep-Tech <sandeeptech8@gmail.com>
2020-11-19 18:28:38 +0530

    =styled CreateButton component

--- webapp/src/HomePage/CreateButton.js
+++ webapp/src/HomePage/CreateButton.js
@@ -1,15 +1,16 @@
-// import "./CreateButton.css";
+import "./CreateButton.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons'
 
 
 const CreateButton = () => {
-  return (<button>
-    <p>Create</p>
+  return (<button className="button">
       
-      <FontAwesomeIcon icon={faCamera} />
-      
-    </button>
+      <FontAwesomeIcon icon={faCamera} id="camera-icon"/>
+
+      <p id="button-text">Create</p>
+    
+      </button>
     );
 };
 
