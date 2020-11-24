9bfc257ca176740a813f435aeaf2f33f44420939
Sandeep-Tech <sandeeptech8@gmail.com>
2020-11-24 18:15:49 +0530

    fixed homepage styling

---
+++ webapp/src/HomePage/CollectionList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import CollectionThumbnail from "./CollectionThumbnail";
+import "./CollectionList.css";
+
+const userCollections = [
+    {
+        image:
+            "https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        title: "Some Wedding",
+        timestamp: "20th December 2020",
+    },
+    {
+        image:
+            "https://images.unsplash.com/photo-1577083753695-e010191bacb5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        title: "Diwali",
+        timestamp: "10th November 2020",
+    },
+    {
+        image:
+            "https://images.unsplash.com/photo-1501426026826-31c667bdf23d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
+        title: "Holidays",
+        timestamp: "10th December 2021",
+    },
+];
+
+
+const CollectionList = () => (
+    <div id="container">
+        {
+            userCollections.map((collection, key) => (
+                <CollectionThumbnail data={collection} key={key} />
+            ))
+        }
+    </div>
+);
+
+export default CollectionList;
--- webapp/src/HomePage/index.js
+++ webapp/src/HomePage/index.js
@@ -1,39 +1,17 @@
 import React from "react";
-import CollectionThumbnail from "./CollectionThumbnail";
 import CreateButton from "./CreateButton";
+import CollectionList from "./CollectionList";
 import "./index.css";
+// import image list component here
 
-const userCollections = [
-  {
-    image:
-      "https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    title: "Some Wedding",
-    timestamp: "20th December 2020",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1577083753695-e010191bacb5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    title: "Diwali",
-    timestamp: "10th November 2020",
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1501426026826-31c667bdf23d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
-    title: "Holidays",
-    timestamp: "10th December 2021",
-  },
-];
 
 class HomePage extends React.Component {
 
   render() {
     return (
       <div className="homepage">
-        {
-          userCollections.map((collection, key) => (
-            <CollectionThumbnail data={collection} key={key} />
-          ))
-        }
+        
+        <CollectionList />
         <CreateButton />
         
 
