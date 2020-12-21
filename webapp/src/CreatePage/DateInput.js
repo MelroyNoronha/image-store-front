ff00109dcc76cd78fb8b60114e463df5026827e9
Sandeep-Tech <sandeeptech8@gmail.com>
2020-12-21 15:37:51 +0530

    date input styling

--- webapp/src/CreatePage/DateInput.js
+++ webapp/src/CreatePage/DateInput.js
@@ -3,17 +3,27 @@ import "./DateInput.css";
 
 const DateInput = () => {
   const currentDate = new Date();
-  const formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`;
+  const formattedDate = `${currentDate.getFullYear()}-${
+    currentDate.getMonth() + 1
+  }-${currentDate.getDate()}`;
   const [date, setDate] = useState(formattedDate);
+
+  // const handleDatePress = () => {
+  //   const dateInput = document.getElementById("date-input");
+  //   console.log("DEBUG", dateInput.focus());
+  //   dateInput.focus(); // PROBLEM-HERE
+  // };
+
   return (
-    <div>
+    <div id="date-input-container">
       <input
         type="date"
         id="date-input"
-        name="trip-start"
+        name="date-input"
         value={date}
         onChange={(event) => setDate(event.target.value)}
       />
+      {/* <p onClick={handleDatePress}>{date}</p> */} 
     </div>
   );
 };
