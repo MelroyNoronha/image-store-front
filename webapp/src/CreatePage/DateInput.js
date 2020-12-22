20aafa756a4eeeecd0f07de03d73e732bfca464c
Sandeep-Tech <sandeeptech8@gmail.com>
2020-12-22 18:16:34 +0530

    changed date formatting

--- webapp/src/CreatePage/DateInput.js
+++ webapp/src/CreatePage/DateInput.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import "./DateInput.css";
-
+import { getHumanFriendlyDate } from "../common/functions/humanFriendlyDate";
 const DateInput = () => {
   const currentDate = new Date();
   const formattedDate = `${currentDate.getFullYear()}-${
     currentDate.getMonth() + 1
   }-${currentDate.getDate()}`;
   const [date, setDate] = useState(formattedDate);
+  const humanFriendlyDate = getHumanFriendlyDate(new Date(date));
 
   // const handleDatePress = () => {
   //   const dateInput = document.getElementById("date-input");
   //   console.log("DEBUG", dateInput.focus());
   //   dateInput.focus(); // PROBLEM-HERE
   // };
-
+  console.log(humanFriendlyDate);
   return (
     <div id="date-input-container">
       <input
@@ -23,7 +24,7 @@ const DateInput = () => {
         value={date}
         onChange={(event) => setDate(event.target.value)}
       />
-      {/* <p onClick={handleDatePress}>{date}</p> */} 
+      {/* <p onClick={handleDatePress}>{date}</p> */}
     </div>
   );
 };
