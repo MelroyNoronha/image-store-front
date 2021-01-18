255ff469450f9e162c41f2d9955cfbfdcafdcbe1
Sandeep-Tech <sandeeptech8@gmail.com>
2021-01-18 18:07:40 +0530

    init setup of idb-key for data persistence

--- webapp/src/redux/store.js
+++ webapp/src/redux/store.js
@@ -1,10 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import collectionReducer from "./reducers/CollectionList";
 import loaderReducer from "./reducers/Loader";
+import { set } from "idb-keyval";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     collection: collectionReducer,
     loader: loaderReducer,
   },
 });
+
+store.subscribe(() => {
+  const currentState = store.getState();
+  const collections = currentState.collection.data;
+  set("collections", collections);
+});
+
+export default store;
