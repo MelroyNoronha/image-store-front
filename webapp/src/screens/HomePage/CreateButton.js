970cf31974e285550b93e94928e0fd8095e40ff7
Sandeep-Tech <sandeeptech8@gmail.com>
2021-02-15 20:37:03 +0530

    refactored the CSS file into a CSS module

--- webapp/src/screens/HomePage/CreateButton.js
+++ webapp/src/screens/HomePage/CreateButton.js
@@ -1,14 +1,14 @@
 import React from "react";
-import "./CreateButton.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import styles from "./CreateButton.module.css";
 
 
 const CreateButton = (props) => {
   return (
-    <button className="button" onClick={props.handleCreatePress}>
-      <FontAwesomeIcon icon={faCamera} id="camera-icon" />
-      <p id="button-text">Create</p>
+    <button className={styles.button} onClick={props.handleCreatePress}>
+      <FontAwesomeIcon icon={faCamera} className={styles.cameraIcon} />
+      <p className={styles.buttonText}>Create</p>
     </button>
   );
 };
