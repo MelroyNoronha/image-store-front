11f3decadd78c8fd6a2d8cd46be9210759803a91
Sandeep-Tech <sandeeptech8@gmail.com>
2021-02-15 20:00:46 +0530

    refactored the main index file css into a module

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -4,7 +4,7 @@ import reportWebVitals from "./reportWebVitals";
 import AppRouter from "./routers/AppRouter";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import "./index.css";
+import "./index.module.css";
 
 ReactDOM.render(
   <React.StrictMode>
